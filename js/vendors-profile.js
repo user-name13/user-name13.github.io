3f425d61bfbe7a2f237340961eff083b2fad50d3
sspakuha <73202124+sspakuha@users.noreply.github.com>
2021-08-02 12:22:43 +0300

    PROFAD

--- js/vendors-profile.js
+++ js/vendors-profile.js
@@ -6,6 +6,30 @@ $(document).ready(function () {
 
 	$('.icon-menu').click(function () {
 		$('.icon-menu, .menu').toggleClass("_active");
-		// $('.menu').toggleClass("_active");
+		$('body').toggleClass("active");
+	})
+
+	$('#menu').hover(function () {
+		$('.hidein').toggleClass("active");
 	})
 })
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop + 3;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+	}
+	else {
+		navbar.classList.remove("sticky");
+	}
+}
